7dbf068641506a8637fc2f1e4f2f3c3d2d2d54e2
Erick Grisoste <grisosteerick25@gmail.com>
2025-03-17 14:38:45 -0300

    Initial commit

---
+++ src/test/java/erick/biotune/BioTune/BioTuneApplicationTests.java
@@ -0,0 +1,13 @@
+package erick.biotune.BioTune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioTuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
